de8670bf7bcbef0428995caee8cbd89e5b3f69cf
Onkar Deshpande <onkarcool123@gmail.com>
2025-02-16 13:08:29 -0500

    feat:pagination work in progress

--- script.js
+++ script.js
@@ -3,17 +3,21 @@
 let previous = document.getElementById("previous");
 let paginationValue = document.getElementById("paginationValue");
 let next = document.getElementById("next");
+const loader = document.getElementById('loader')
+
+const paginationListHolder = document.getElementById('pagination-list-holder')
+
 
 let inputPage = 1;
 let inputLimit;
 let count = 0;
-let pageNum = document.getElementById("pageNum");
+// let pageNum = document.getElementById("pageNum");
 let limit = document.getElementById("limitOption");
-pageNum.addEventListener("input", (e) => {
-  inputPage = e.target.value;
-  paginationValue.innerText = inputPage;
-  fetchingApi();
-});
+// pageNum.addEventListener("input", (e) => {
+//   inputPage = e.target.value;
+//   paginationValue.innerText = inputPage;
+//   fetchingApi();
+// });
 limit.addEventListener("input", (e) => {
   inputLimit = e.target.value;
   if (inputLimit > 34) {
@@ -24,13 +28,27 @@ limit.addEventListener("input", (e) => {
 });
 fetchingApi();
 
+const createPaginationNumberBtn = (linkObj, currentPage) => {
+  const linkAnchor = document.createElement('a')
+  linkAnchor.href = linkObj.url
+  linkAnchor.innerText = linkObj.label
+  linkAnchor.style.padding = '1rem'
+  if (linkObj.active) {
+    linkAnchor.style.border = '1px solid black'
+  }
+  linkAnchor.target = '_blank'
+  return linkAnchor
+}
+
+
 async function fetchingApi() {
+  loader.style.display = 'block'
   const params = new URLSearchParams({
     limit: 10,
     // offset: 6 * 10,
   });
   let firstResponse = await fetch(
-    `https://catfact.ninja/facts?${params.toString()}&page=${inputPage}`,
+    `https://catfact.ninja/facts?${params.toString()}&page=${10}`,
     {
       method: "GET",
       headers: {
@@ -38,13 +56,15 @@ async function fetchingApi() {
       },
     }
   );
-  let secondResponse = await firstResponse.json();
-  let responseData = secondResponse.data;
+  let catFactsResponseData = await firstResponse.json();
+  let responseData = catFactsResponseData.data;
   let para = document.getElementById("mainSection");
   para.innerHTML = " ";
   let ul = document.createElement("ul");
   para.appendChild(ul);
   sliceLimit = responseData.slice(0, inputLimit || 10);
+  loader.style.display = 'none'
+
   sliceLimit.forEach((info) => {
     let li = document.createElement("li");
     li.setAttribute("style", "margin:15px");
@@ -52,7 +72,13 @@ async function fetchingApi() {
     li.innerText = info.fact;
   });
 
-  console.log(responseData.slice(0, inputLimit));
+  const paginationLinks = catFactsResponseData.links
+  const currentPage = catFactsResponseData.current_page
+  paginationLinks.forEach((link) => {
+    const paginationAnchorElement = createPaginationNumberBtn(link, currentPage)
+    paginationListHolder.appendChild(paginationAnchorElement)
+  }
+  )
 }
 
 previous.addEventListener("click", () => {
